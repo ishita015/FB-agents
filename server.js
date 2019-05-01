b4b0ce8d6aa7882cce1569340ec7a6bae540fe43
ishita pardeshi <ishitapardeshi0@gmail.com>
2019-05-01 15:53:15 +0530

    3332

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = require('./app');
 const mongoose = require('mongoose');
 const config = require('./config');
